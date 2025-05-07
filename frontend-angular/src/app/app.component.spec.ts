5819058caa7bec4a0bdba6d283eca8b47ab77674
martineviden <martin.guijarro.external@eviden.com>
2025-05-07 13:44:29 +0200

    first commit

--- frontend-angular/src/app/app.component.spec.ts
+++ frontend-angular/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'proyecto-concesionario' title`, () => {
+  it(`should have the 'frontend-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyecto-concesionario');
+    expect(app.title).toEqual('frontend-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyecto-concesionario');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontend-angular');
   });
 });
