03be901589c790b8234f1559eae8f6baee7b1a21
martineviden <martin.guijarro.external@eviden.com>
2025-05-07 13:37:06 +0200

    initial commit

---
+++ backend-springboot/src/main/java/com/atos/concesionario/proyecto_concesionario/ProyectoConcesionarioApplication.java
@@ -0,0 +1,13 @@
+package com.atos.concesionario.proyecto_concesionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoConcesionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoConcesionarioApplication.class, args);
+	}
+
+}
