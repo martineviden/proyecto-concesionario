b111420dfe6eb6924b704684a489a19358129a83
camilo1102 <camiloandretti1102@gmail.com>
2025-05-07 16:52:56 +0200

    Entidades Creadas / Model

---
+++ backend-springboot/src/main/java/com/atos/concesionario/proyecto_concesionario/Model/Usuario.java
@@ -0,0 +1,38 @@
+package com.atos.concesionario.proyecto_concesionario.Model;
+
+import lombok.*;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String contraseña;
+    private String telefono;
+    private String fotoPerfil;
+
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Reserva> reservas;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Reseña> reseñas;
+
+    public enum Rol {
+        CLIENTE, ADMIN
+    }
+}
