14b0976154279cccc4cb3fcd67f92337aeba9d7a
camilo1102 <camiloandretti1102@gmail.com>
2025-05-13 12:08:49 +0200

    mas cambios/angular

--- frontend-angular/concesionario/src/app/app.routes.ts
+++ frontend-angular/concesionario/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './shared/components/login/login.component';
+import { RegistrarseComponent } from './shared/components/registrarse/registrarse.component';
+import { VehiculosComponent } from './shared/components/vehiculos/vehiculos.component';
+import { TipoVehiculoComponent } from './shared/components/tipo-vehiculo/tipo-vehiculo.component';
+import { ReservaFormComponent } from './shared/components/reserva-form/reserva-form.component';
+import { MisReservasComponent } from './shared/components/mis-reservas/mis-reservas.component';
+import { AdminReservasComponent } from './shared/components/admin-reservas/admin-reservas.component';
+import { ResenaFormComponent } from './shared/components/resena-form/resena-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'vehiculos', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'registrarse', component: RegistrarseComponent },
+  { path: 'vehiculos', component: VehiculosComponent },
+  { path: 'tipo-vehiculo/:id', component: TipoVehiculoComponent },
+  { path: 'reservar/:matricula', component: ReservaFormComponent },
+  { path: 'mis-reservas', component: MisReservasComponent },
+  { path: 'admin/reservas', component: AdminReservasComponent },
+  { path: 'resena-form', component: ResenaFormComponent}
+
+];
